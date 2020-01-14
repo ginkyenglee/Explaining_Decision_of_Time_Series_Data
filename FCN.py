e61ff0a446c877ca2b9faf2c4a6167bea69d3189
ginkyenglee <jinkyung908@gmail.com>
2020-01-15 00:19:06 +0900

    add model structure and code for turning off last convolution output for threshod

--- FCN.py
+++ FCN.py
@@ -1,5 +1,6 @@
 from tensorflow.python.ops import nn_ops, gen_nn_ops
 import tensorflow as tf
+import numpy as np
 
 class CNN_MC_dropout:
     def __init__(self, std=0.01, batch_size=64,width=500, height =1, input_channel=3, nb_classes=2, l_rate =1e-6,reuse = False):
@@ -104,6 +105,129 @@ class CNN_MC_dropout:
                              feed_dict={self.x: data,
                                        self.keep_prob: keep_prob,
                                        self.is_dropout : is_dropout})
+        return prediction
+
+    def save(self, save_path='./model.ckpt'):
+        saved_path = self.saver.save(self.sess, save_path)
+        print("Model saved in file: %s"%saved_path)
+
+    def load(self, load_path = './model.ckpt'):
+        self.saver.restore(self.sess, load_path)
+        print("Model restored")
+
+    def terminate(self):
+        self.sess.close()
+        tf.reset_default_graph() 
+        
+class CNN_MC_dropout_turnoff:
+    def __init__(self, std=0.01, batch_size=64,width=500, height =1, input_channel=3, nb_classes=2, l_rate =1e-6,reuse = False):
+        self.std=std
+        self.batch_size=batch_size
+        self.height = height
+        self.width = width
+        self.input_channel = input_channel
+        self.l_rate = l_rate
+        
+        self.nb_classes = nb_classes
+
+        with tf.name_scope('Classifier'):
+            self.y = tf.placeholder(tf.float32, [None, self.nb_classes], name='y')
+            self.x = tf.placeholder(tf.float32, [None, self.height,self.width,self.input_channel], name='x')
+            self.keep_prob = tf.placeholder(tf.float32)
+            self.is_dropout = tf.placeholder(tf.bool)
+            self.threshold_weight = tf.placeholder(tf.float32, [None,1,128,128])
+        
+        # keep prob가 1보다 작으면, 드랍아웃을 한다는 의미
+        #self.dropout_bool = True#tf.cond(self.keep_prob < 1.0, lambda: tf.constant(True), lambda: tf.constant(False))
+        
+        self.logits = self.build_model()
+
+        # Define loss and optimizer, minimize the squared error
+        self.cross_entropy =  tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.y, logits=self.logits)
+        self.cost =tf.reduce_mean(self.cross_entropy)
+        self.optimizer = tf.train.AdamOptimizer(self.l_rate).minimize(self.cost)
+
+        self.correct_pred = tf.equal(tf.round(self.prediction),self.y)
+        self.accuracy = tf.reduce_mean(tf.cast(self.correct_pred, tf.float32))
+
+        init = tf.global_variables_initializer()
+
+        # Launch the session
+        self.session_conf = tf.ConfigProto()
+        self.session_conf.gpu_options.allow_growth = True
+        self.sess = tf.InteractiveSession(config=self.session_conf)
+        self.sess.run(init)
+
+        self.saver = tf.train.Saver(max_to_keep=None)
+
+    def build_model(self):
+        with tf.variable_scope('layer0'):
+            #b, h, w, c
+            self.input = self.x
+        # Convolutional Layer #1 and Pooling Layer #1
+        with tf.variable_scope('layer1'):
+            self.conv1 = tf.layers.conv2d(self.input, 128, [8,1], padding='SAME')
+            self.batch1 = tf.layers.batch_normalization(self.conv1)
+            self.relu1 = tf.nn.relu(self.batch1)
+            self.dropout1 = tf.nn.dropout(self.relu1, self.keep_prob)
+
+        # Convolutional Layer #1 and Pooling Layer #2
+        with tf.variable_scope('layer2'):
+            self.conv2 = tf.layers.conv2d(self.dropout1, 256, [5,1], padding='SAME')
+            self.batch2 = tf.layers.batch_normalization(self.conv2)
+            self.relu2 = tf.nn.relu(self.batch2)
+            self.dropout2 = tf.nn.dropout(self.relu2, self.keep_prob)
+
+        # Convolutional Layer #1 and Pooling Layer #3
+        with tf.variable_scope('layer3'):
+            self.conv3 = tf.layers.conv2d(self.dropout2, 128, [3,1], padding='SAME')
+            self.batch3 = tf.layers.batch_normalization(self.conv3)
+            self.relu3 = tf.nn.relu(self.batch3)
+            self.threshold_relu3 = tf.multiply(self.relu3 , self.threshold_weight)
+
+        # Dense Layer with Relu
+        with tf.variable_scope('layer4'):
+            #Global Average Pooling
+            self.GAP = tf.reduce_mean(self.threshold_relu3, axis=[1,2])# b,h,w,c
+            self.logits = tf.layers.dense(self.GAP,self.nb_classes)
+            self.prediction = tf.nn.softmax(self.logits)
+            
+        return self.logits
+
+
+    def train(self, data, target, keep_prob,is_dropout):
+        opt, cost ,acc = self.sess.run((self.optimizer, self.cost, self.accuracy ), 
+                             feed_dict={self.y: target,
+                                        self.x: data,
+                                       self.keep_prob: keep_prob,
+                                       self.is_dropout : is_dropout,
+                                       self.threshold_weight : np.ones(data.shape[0]*128*128).reshape(-1,1,128,128)})
+        return cost,acc
+
+    def test(self, data, target, keep_prob,is_dropout):
+        cost,acc = self.sess.run((self.cost,self.accuracy),
+                             feed_dict={self.y: target,
+                                        self.x: data,
+                                       self.keep_prob: keep_prob,
+                                       self.is_dropout : is_dropout,
+                                       self.threshold_weight :np.ones(data.shape[0]*128*128).reshape(-1,1,128,128)})
+        return cost,acc
+
+    def get_last_conv_output(self, data, keep_prob,is_dropout):
+        relu3 =  self.sess.run((self.relu3), 
+                             feed_dict={self.x: data,
+                                       self.keep_prob: keep_prob,
+                                       self.is_dropout : is_dropout,
+                                       self.threshold_weight : np.ones(data.shape[0]*128*128).reshape(-1,1,128,128)})
+        return relu3
+    
+    def predict(self, data, keep_prob,is_dropout,threshold_weight):
+
+        prediction =  self.sess.run((self.prediction), 
+                             feed_dict={self.x: data,
+                                       self.keep_prob: keep_prob,
+                                       self.is_dropout : is_dropout,
+                                       self.threshold_weight : threshold_weight})
         return prediction
 
     def save(self, save_path='./model.ckpt'):
